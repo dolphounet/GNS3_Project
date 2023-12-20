c47cbe1677b9216cfa4dbcf6cf7a13f6036d1293
MadMuses <mamusique128@gmail.com>
2023-12-20 17:53:41 +0100

    fonction de création de matrice d'adjascence

--- IPattribution.py
+++ IPattribution.py
@@ -13,16 +13,12 @@ def createMatAdj(network):
         for j in range(0,N):
             adjMat[i].append(0)
 
-    
-    
+    for router in network["routers"]:
+
+        for interface in router["interface"]:
+
+            for i in range(0,len(interface[0])):
+                if interface[0][i] != []:
+                    adjMat[router["name"]-1][interface[0][i]-1] = 1
+
     return adjMat
-
-
-
-
-def test():
-    network = filesIO.read_from_json('Network_Intent.json')
-    print(createMatAdj(network))
-    return 0
-
-test()
