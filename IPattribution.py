63541a8cc0b8815e6b28b4e1adc779a4f9c476c1
MadMuses <mamusique128@gmail.com>
2023-12-22 09:40:20 +0100

    Last wrong commit

--- IPattribution.py
+++ IPattribution.py
@@ -3,17 +3,34 @@ import filesIO
 def attributionIP(network):
     pass
 
-def createMatAdj(network):
-
-    N = len(network["routers"])
+def findAdjacency(network):
     adjDic = {}
 
     for router in network["routers"]:
-        adjDic[str(router["ID"][0])]=[]
+        adjDic[router["ID"][0]]=[]
 
         for interface in router["interface"]:
             for i in range(0,len(interface[0])):
                 if interface[0][i] != []:
-                    adjDic[str(router["ID"][0])].append(interface[0][i])
+                    adjDic[router["ID"][0]].append(interface[0][i])
 
     network["adjDic"] = adjDic
+
+    return
+
+def createLinks(network):
+    visited = []
+
+    linkCountAS = []
+    for i in range(0,len(network["AS"])):
+        linkCountAS.append(0)
+
+    for routerID in network["adjDic"] :
+        visited.append(routerID)
+
+        for connectedRouter in network["adjDic"][routerID]:
+            if (network["routers"][connectedRouter-1]["AS"] == network["routers"][routerID-1]["AS"]) and (connectedRouter not in visited):
+                
+                linkCountAS[network["routers"][routerID-1]["AS"]-1] += 1
+
+    
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from filesIO import read_from_json
-from IPattribution import createMatAdj
+from IPattribution import findAdjacency, createLinks
 
 
 def main():
     network = read_from_json('Network_intent.json')
-    #print(network["AS"])
-    createMatAdj(network)
+    for router in network["routers"]:
+        print(router)
+
+    findAdjacency(network)
     print(network["adjDic"])
+    
+    createLinks(network)
 
 if __name__ == "__main__":
     main()
