8e5ab753e91e7af8628f54470b3a8a6cb44e7c7e
Madmuses <mamusique128@gmail.com>
2024-01-11 11:49:52 +0100

    Implemented drag bot for cfg files

--- filesIO.py
+++ filesIO.py
@@ -1,4 +1,6 @@
 import json
+import os
+from networkConfig import config_router
 
 def readJson(file):
     Network_Intent = open(file)
@@ -6,7 +8,19 @@ def readJson(file):
     Network_Intent.close
     return network
 
-def writeCfg(file, config):
-    with open(file, "w") as configFile:
+def writeCfg(filePath, config):
+    with open(filePath, "w") as configFile:
         configFile.write(config)
     return
+
+def placeBot(network,projectPath):
+    #projectPath += "/project-files/dynamips"
+    filesPath = {}
+
+    for dirPath, dirs, files in os.walk(projectPath):
+        for file in files:
+            if file.endswith('.cfg'):
+                filesPath[file] = os.path.join(dirPath, file)
+
+    for router in network["routers"]:
+        writeCfg(filesPath[f'i{router["ID"][0]}_startup-config.cfg'], config_router(network, router["ID"][0]))
