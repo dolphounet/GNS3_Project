dd628d2d807473b9d605cc1c9f3e0817dd6390fb
Madmuses <mamusique128@gmail.com>
2024-01-11 11:50:36 +0100

    Main function now uses the bot to generate .cfg files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from filesIO import readJson, writeCfg
+from filesIO import readJson, placeBot
 from IPattribution import attributeIP
-from networkConfig import config_router
+
 
 
 def main():
@@ -11,12 +11,8 @@ def main():
     # Attribution des IP
     attributeIP(network)
 
-    #print(network)
-    
     # Ecriture des fichiers config
-    for router in network["routers"]:
-        writeCfg(f'i{router["ID"][0]}_startup-config.cfg', config_router(network, router["ID"][0]))
-
+    placeBot(network,input("Input a file path : "))
 
 if __name__ == "__main__":
     main()
