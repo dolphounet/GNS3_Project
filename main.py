80e8667aac898ef652de01b83ae08fc658eaf817
MadMuses <mamusique128@gmail.com>
2024-01-19 16:48:46 +0100

    Removed testing prints

--- main.py
+++ main.py
@@ -11,25 +11,15 @@ def main():
 
     # Récupération des informations du réseau
     network = readJson('Network_Intent.json')
-    '''
-    for elem in network["AS"]:
-        print(elem)
-    '''
+    
     # Attribution des IP
     attributeIP(network)
 
     # Ecriture des fichiers config
     placeBot(network,path)
-    '''
-    for router in network["routers"]:
-        writeCfg(f'output/i{router["ID"][0]}_startup-config.cfg', config_router(network, router["ID"][0]))
-    '''
+
     for elem in network["InterAS"]["InterASlinks"]["Links"]:
         print(f'{elem} : {network["InterAS"]["InterASlinks"]["Links"][elem]}')
-
-    
-    
-    
 
 if __name__ == "__main__":
     main()
