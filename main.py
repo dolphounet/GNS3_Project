51d5d9a69877aee40b6c16e0e4f5097e2b8e824f
dolphounet <maxencepj.04@gmail.com>
2024-01-24 09:55:49 +0100

    parallelized the code for faster execution

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from filesIO import readJson
 from IPattribution import attributeIP
 from networkConfig import config_router
+import threading
 
 
 
 def main():
 
     # Récupération des informations du réseau
-    network = readJson('Network_Intent.json')
+    network = readJson('Network_Intent_comm.json')
 
     # Attribution des IP
     attributeIP(network)
 
     # Ecriture de la configuration avec telnet
-    for router in network["routers"]:
-        config_router(network, router["ID"][0])
+    
+    threads = [threading.Thread(target=config_router, args=(network, i+1)) for i in range(len(network["routers"]))]
+    for thread in threads:
+        thread.start()
 
+    for thread in threads :
+        thread.join()
 if __name__ == "__main__":
     main()
 
